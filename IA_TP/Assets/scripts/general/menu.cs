c856f719f29b277380a6e57f41ecb904fa1d8cc9
mariano <marianoa.munguia@gmail.com>
2021-05-04 11:29:57 -0300

    ui, escenas extras, menus y botones

---
+++ IA_TP/Assets/scripts/general/menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    public void ClickInBoton(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
