100d912dee8034b586c8b425a275f1dd1d6b16e5
mariano <marianoa.munguia@gmail.com>
2021-05-09 09:50:24 -0300

    ajuste tamaÑ0s

---
+++ IA_TP/Assets/scripts/enemy/Seek.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : ISteering
+{
+    Transform _target;
+    Transform _transform;
+    float _distance;
+
+    public Seek(Transform to, Transform from, float dist)
+    {
+        _target = to;
+        _transform = from;
+        _distance = dist;
+    }
+
+    public Vector3 GetDirection()
+    {
+        if (Vector3.Distance(_target.position, _transform.position) > _distance)
+        {
+            Vector3 direction = (_target.position - _transform.position).normalized;
+            return direction;
+        }
+        else return Vector3.zero;
+    }
+}
