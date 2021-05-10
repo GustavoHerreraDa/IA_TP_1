1c708ee3c0e7bdff29f01a8876a64d54df89b4cb
GustavoHerreraDa <gustavo.herrera@davinci.edu.ar>
2021-05-10 05:28:10 -0300

    FSM - Initial Code with EnemyController.

---
+++ IA_TP/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    QuestionNode _questionSight;
+    QuestionNode _questionTimeToShoot;
+    QuestionNode _questionRoom;
+    ActionNode _actionIdle;
+    ActionNode _actionShoot;
+    ActionNode _actionMove;
+
+    INode _init;
+    
+    FSM<string> finateStateMachine;
+    IState<string> idle;
+    IState<string> attack;
+    IState<string> patrol;
+    IState<string> chase;
+
+
+    Player player;
+
+    private Rigidbody myRigidbody;
+
+    public Transform firepoint;
+    public float dodgeStrenght;
+    public float dodgeRadious;
+    private WalkToPoints walkToPoints;
+
+    private void Awake()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        walkToPoints = GetComponent<WalkToPoints>();
+    }
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+
+        idle = new IA_Idle<string>(this);
+        patrol = new IA_Patrol<string>(this, myRigidbody, walkToPoints);
+        chase = new IA_Chase<string>(this, myRigidbody, dodgeStrenght, dodgeRadious);
+        attack = new IA_Attack<string>(this, firepoint);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTree()
+    {
+        _init.Execute();
+    }
+}
---
+++ IA_TP/Assets/Scripts/StateMachine/IA_Chase.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_Chase<T> : IState<T>
+{
+    Dictionary<T, IState<T>> dictionaryState = new Dictionary<T, IState<T>>();
+    EnemyController enemy;
+    Rigidbody enemyRigidbody;
+    Transform playerTransform;
+    //Seek seek;
+    float dodgeStrenght = 10;
+    float dodgeRadius = 20;
+    Vector3 dir;
+    float speed = 2;
+
+    public IA_Chase(EnemyController enemy, Rigidbody rb, float ds = 10, float dr = 20, float s = 7)
+    {
+        this.enemy = enemy;
+        enemyRigidbody = rb;
+        dodgeStrenght = ds;
+        dodgeRadius = dr;
+        speed = s;
+    }
+
+    public void Awake()
+    {
+        playerTransform = Object.FindObjectOfType<Player>().transform;
+        //seek = new Seek(_enemy.transform, playerT, dodgeStrenght, dodgeRadius);
+    }
+
+    public void Execute()
+    {
+        Move();
+        enemy.StartTree();
+    }
+
+    public void Sleep()
+    {
+
+    }
+
+    public void AddTransition(T input, IState<T> state)
+    {
+        if (!dictionaryState.ContainsKey(input))
+            dictionaryState.Add(input, state);
+    }
+    public void RemoveTransition(T input)
+    {
+        if (dictionaryState.ContainsKey(input))
+            dictionaryState.Remove(input);
+    }
+    public IState<T> GetState(T input)
+    {
+        if (dictionaryState.ContainsKey(input))
+            return dictionaryState[input];
+        return null;
+    }
+    public void Move()
+    {
+        //dir = seek.GetDir();
+        dir.y = 0;
+        dir = dir.normalized;
+        enemyRigidbody.velocity = dir * speed;
+        enemy.transform.forward = Vector3.Lerp(enemy.transform.forward, dir, 0.2f);
+    }
+}
